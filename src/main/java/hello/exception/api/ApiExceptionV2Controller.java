6c55a0fc0babec8d5fb32ed7524703467b799036
ssm <tjtkdan1072@gmail.com>
2025-10-17 14:10:02 +0900

    API 예외 처리 - 스프링 @ControllerAdive 적용하기

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -13,27 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = {IllegalArgumentException.class})
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.info("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.info("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandle(Exception e) {
-        log.info("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", e.getMessage());
-    }
-
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
